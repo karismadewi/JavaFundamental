9061b809f9a3a4bc5604256de54b334eb786d1c9
karismadewi <risma.karisma753@gmail.com>
2022-11-05 10:58:47 +0800

    JF_V02_S03_LP05PrjStudent

--- Greenfoot/JF-V02_S03_P01PrjStudent/RobotWorld.java
+++ Greenfoot/JF-V02_S03_P01PrjStudent/RobotWorld.java
@@ -19,13 +19,61 @@ public class RobotWorld extends World
         super(800, 600, 1); 
         prepare();
     }
+    
     /**
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
     private void prepare()
     {
+
+        Robot robot = new Robot();
+        addObject(robot,48,50);
+
         block block = new block();
-        addObject(block,387,106);
+        addObject(block,427,145);
+
+        wall wall = new wall();
+        addObject(wall,48,50);
+        wall.setLocation(48,147);
+
+        wall wall2 = new wall();
+        addObject(wall2,52,147);
+
+        wall wall3 = new wall();
+        addObject(wall3,159,147);
+
+        wall wall4 = new wall();
+        addObject(wall4,266,147);
+
+        wall wall5 = new wall();
+        addObject(wall5,587,147);
+
+        wall wall6 = new wall();
+        addObject(wall6,692,147);
+
+        wall wall7 = new wall();
+        addObject(wall7,791,147);
+
+        home home = new home();
+        addObject(home,751,552);
+
+        ScorePanel scorePanel = new ScorePanel();
+        addObject(scorePanel,71,554);
+        
+        pizza pizza = new pizza();
+        addObject(pizza,720,46);
+        
+        pizza pizza2 = new pizza();
+        addObject(pizza2,443,38);
+        
+        pizza pizza3 = new pizza();
+        addObject(pizza3,183,302);
+        
+        pizza pizza4 = new pizza();
+        addObject(pizza4,682,312);
+        
+        pizza pizza5 = new pizza();
+        addObject(pizza5,417,537);
     }
 }
